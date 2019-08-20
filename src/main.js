e2f6c5dcd3b242286c5e1f04c5ee128c88cb4b13
unknown <okumura6883@gmail.com>
2019-08-20 01:01:14 -0700

    3D background and light/night mode

--- src/main.js
+++ src/main.js
@@ -1,38 +1,50 @@
 const $ = require("jquery");
-const THREE = require("three");
+const PIXI = require("pixi.js");
 
-let camera, scene, renderer, geometry, material, mesh;
- 
-const init = () => {
- 
-    camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 0.01, 10);
-    camera.position.z = 1;
- 
-    scene = new THREE.Scene();
- 
-    geometry = new THREE.BoxGeometry(0.2, 0.2, 0.2);
-    material = new THREE.MeshNormalMaterial();
- 
-    mesh = new THREE.Mesh(geometry, material);
-    scene.add(mesh);
- 
-    renderer = new THREE.WebGLRenderer({ antialias: true });
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    document.body.appendChild(renderer.domElement);
- 
-}
- 
-const animate = () => {
+$(window).on("load", () => {
+    
+    let app = new PIXI.Application({ width: window.innerWidth, height: window.innerHeight });
+    document.body.append(app.view);
 
-    requestAnimationFrame(animate);
- 
-    mesh.rotation.x += 0.01;
-    mesh.rotation.y += 0.02;
- 
-    renderer.render(scene, camera);
-}
+    app.view.style.position = "fixed";
+    app.view.style.top = "0px";
+    app.view.style.left = "0px";
+    app.view.style.zIndex = "-99";
 
-$(document).ready(() => {
-    init();
-    animate();
+    let lightBackground = PIXI.Sprite.from($("#light-texture")[0]);
+    let nightBackground = PIXI.Sprite.from($("#night-texture")[0]);
+
+    app.stage.addChild(nightBackground);
+    app.stage.addChild(lightBackground);
+
+    let textureMap = new PIXI.Sprite.from($("#texture-map")[0]);
+
+    app.stage.addChild(textureMap);
+
+    let depthFilter = new PIXI.filters.DisplacementFilter(textureMap);
+    app.stage.filters = [ depthFilter ];
+
+    $(window).mousemove(e => {
+        depthFilter.scale.x = (window.innerWidth / 2 - e.clientX) / 80; 
+        depthFilter.scale.y = (window.innerHeight / 2 - e.clientY) / 80; 
+    });
+
+    const lightMode = () => lightBackground.alpha >= 1 ? lightBackground.alpha = 1 :
+        (lightBackground.alpha += 0.05, requestAnimationFrame(lightMode));
+
+    
+    const nightMode = () => lightBackground.alpha <= 0 ? lightBackground.alpha = 0 :
+        (lightBackground.alpha -= 0.05, requestAnimationFrame(nightMode));
+
+    $("#toggle").click(function() {
+        if (this.textContent === "Night Mode") {
+
+            nightMode();
+            this.textContent = "Light Mode";
+        } else {
+            
+            lightMode();
+            this.textContent = "Night Mode";
+        }
+    })
 });
